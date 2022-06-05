cd814478ebee0769cb0a42a88ef4adb20ec117bf
omids0 <soheilniao@gmail.com>
2022-06-05 23:14:58 +0430

    datagride added to right side

--- src/components/RightSide.js
+++ src/components/RightSide.js
@@ -1,7 +1,36 @@
 import { Stack } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 import React from "react";
 
 export default function RightSide({ data }) {
-  console.log(data);
-  return <Stack px={1}>right</Stack>;
+  const columns = [
+    { field: "countryName", headerName: "Country", width: 100 },
+    { field: "cases", headerName: "Cases", width: 60 },
+    { field: "recovered", headerName: "Recovered", width: 85 },
+    { field: "deaths", headerName: "Deaths", width: 65 },
+  ];
+
+  const dataRows = data.map(
+    (item) => 
+    {
+      return {
+        'id': `${item.countryInfo._id}`,
+        'countryName': `${item.country}`,
+        'cases': `${item.todayCases}`,
+        'recovered': `${item.todayRecovered}`,
+        'deaths': `${item.todayDeaths}`
+      }
+    }
+  );
+
+  return (
+    <Stack px={1} bgcolor='#ffffff' py={1} sx={{ minHeight: "700px" }}>
+      <DataGrid
+        rows={dataRows}
+        columns={columns}
+        pageSize={10}
+        rowsPerPageOptions={[10]}
+      />
+    </Stack>
+  );
 }
